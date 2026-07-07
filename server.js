dc02d3a57d20dd81ff6f35a8caf93670c2958f0d
clientesplaytv <clientesplaytv24@gmail.com>
2026-07-07 10:06:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => res.send('Robô de Monitoramento de Fotos Ativo!'));
 app.listen(process.env.PORT || 3000, () => console.log('🌐 Servidor Web ativo!'));
 
 // Seu número configurado no formato oficial interno:
-const NUMERO_DO_ROBO = "554598161585"; 
+const NUMERO_DO_ROBO = "5545998161585"; 
 
 let dadosDoDia = {};
 
