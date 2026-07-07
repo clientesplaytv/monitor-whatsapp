d9a910545ced0a812724a035870a248771ed589b
clientesplaytv <clientesplaytv24@gmail.com>
2026-07-07 16:52:56 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+module.exports = {
+
+    PORT: process.env.PORT || 3000,
+
+    TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
+
+    TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+
+    LIMITE_POSTAGENS: 3,
+
+    LIMITE_FOTOS: 10,
+
+    TEMPO_ALBUM: 2000
+
+};
